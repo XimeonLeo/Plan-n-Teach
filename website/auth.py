be3da83bc63e2536e4bc0d17f8e97a101d79ff6a
Adetops <heritagediwamu@gmail.com>
2024-04-01 08:17:08 +0100

    auth update

--- website/auth.py
+++ website/auth.py
@@ -11,7 +11,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-  if request.method=='POST':
+  if request.method == 'POST':
     email = request.form.get('email')
     password = request.form.get('password')
     
